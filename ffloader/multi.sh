e0a34a2ed634791e5e9885c3e3b989509c306b20
JaeHo Song <thruthesky@gmail.com>
2023-03-16 22:54:17 +0800

    doc

---
+++ ffloader/multi.sh
@@ -0,0 +1,18 @@
+project=$1
+keys=("../keys/flutterflow.0.key" "../keys/flutterflow.1.key" "../keys/flutterflow.2.key")
+
+while true
+do
+    for file in "${keys[@]}"; do
+        key=`cat $file`
+        now=`date`
+        echo "Downloading at: $now / key: $key"
+        SECONDS=0
+        flutterflow export-code --project $project --dest ./apps/$project --include-assets --token $key
+        duration=$SECONDS
+        echo "--> Downloaded in $(($duration / 60)) minutes and $(($duration % 60)) seconds."
+        kill -USR2 $(cat ./flutter.pid)
+        sleep 30
+    done
+done
+
