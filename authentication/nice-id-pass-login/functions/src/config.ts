5d361d79f036064e88e4a8890cb2f718dedaeb97
JaeHo Song <thruthesky@gmail.com>
2023-03-18 11:53:41 +0800

    update comments & docs"

--- authentication/nice-id-pass-login/functions/src/config.ts
+++ authentication/nice-id-pass-login/functions/src/config.ts
@@ -23,17 +23,21 @@ export class Config {
   // 리턴 URL, 콜백 URL. 프로젝트 마다 적절히 수정해야 한다. 특히, domain(host) 부분.
   // 웹 표준 인증 창에서 인증을 한 다음, 인증 정보를 받을 callback url.
   // 이 함수에서 인증 정보를 받아서, 복호화 하여 Custom Token 을 생성한다.
-  static returnUrl = Config.local
-    ? "http://localhost:5001/hype-9f920/asia-northeast3/niceAuthCallback"
-    : "https://asia-northeast3-hype-9f920.cloudfunctions.net/niceAuthCallback";
+  static returnUrl = Config.local ?
+    "http://localhost:5001/hype-9f920/asia-northeast3/niceAuthCallback" :
+    "https://asia-northeast3-hype-9f920.cloudfunctions.net/niceAuthCallback";
 
   // niceAuthCallback 에서 사용자 정보 복호화 후, Custom token 으로 앱을 열기 위한, Deep Link URL.
-  // Link 생성 참조: https://docs.google.com/document/d/1jJnrcPuSWBTzmSQE6y4gz3pO8F743gPy1JO9sNOQiCU/edit#heading=h.4bz1aeelm7q6
+  //
+  // 앱을 실행하고 /afterNiceAuth 로 custom token 과 함께 이동한다. (그래서, 앱/웹에 /afterNiceAuth route 가 있어야 하고, custom token 을 받아서 로그인을 해야 한다.)
+  //
   // `PatchToken` 에 토큰이 들어간다.
-  // 모든 것이 성공적으로 인증되면, 앱의 /afterNiceAuth route 로 접속을 한다.
-  static dynamicLink = Config.local
-    ? "http://localhost:53538/afterNiceAuth/?token="
-    : "https://hypetalk.page.link/?link=https://hypetalk.page.link/afterNiceAuth/?token%3DPatchToken&apn=com.withcenter.hypetalk&isi=6446163804&ibi=com.withcenter.hypetalk&st=Auth+Callback&sd=Nice+Auth+Callback+URL&efr=1";
+  //
+  // Link 생성 참조: https://docs.google.com/document/d/1jJnrcPuSWBTzmSQE6y4gz3pO8F743gPy1JO9sNOQiCU/edit#heading=h.4bz1aeelm7q6
+  // 플러터플로 작업 참조: https://docs.google.com/document/d/1jJnrcPuSWBTzmSQE6y4gz3pO8F743gPy1JO9sNOQiCU/edit#heading=h.r4nz8bt860ok
+  static dynamicLink = Config.local ?
+    "http://localhost:53538/afterNiceAuth/?token=" : // "https://hypetalk.page.link/zXbp?token=xxxx";
+      "https://hypetalk.page.link/?link=https://hypetalk.page.link/afterNiceAuth/?token%3DPatchToken&apn=com.withcenter.hypetalk&isi=6446163804&ibi=com.withcenter.hypetalk&st=하입톡&sd=프라이빗한+메신저를+만나다";
 
   //
   // -- 아래는 경우에 따라서 수정해야 한다. --
--- authentication/nice-id-pass-login/functions/src/index.ts
+++ authentication/nice-id-pass-login/functions/src/index.ts
@@ -1,42 +1,58 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import { Config } from "./config";
-import { NiceApi } from "./nice-api";
-import { AuthData } from "./nice-api.interfaces";
-import { Firebase } from "./firebase";
-import { UserData } from "./firebase.interface";
+import {Config} from "./config";
+import {NiceApi} from "./nice-api";
+import {AuthData} from "./nice-api.interfaces";
+import {Firebase} from "./firebase";
+import {UserData} from "./firebase.interface";
 
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
 });
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+/**
+ * 앱에서 로그인 또는 인증하기 버튼을 클릭하면 호출되는 함수.
+ *
+ * 먼저, 고정 IP 를 사용해야만 한다. 클라우드 함수는 기본적으로 동적 IP 를 사용한다.
+ * 그래서 처음에는 잘 되나가 어느 순간 부터는 인증창이 안열리는 상황이 발생하므로 꼭 고정 IP 를 사용해야 한다.
+ *
+ * response.redirect() 를 하면 40% 확률 본인 인증 실패한다. (표준 인증창이 열리지 않는다.) 정확한
+ * 이유는 모르겠지만, redirect 할 때 어떤 상황/시점/조건에 따라서 실패하는 것 같다. (정말 알 수 없는 일이다.)
+ * FORM 을 만들고 전송을 하니, 100% 정상 동작한다. 실패가 단 한건도 없이, 표준 인증창을 잘 열 수 있었다.
+ */
 export const niceAuth = functions
-  // VPC Connector 를 사용해서 고정 IP 를 사용한다.
-  .runWith({
-    vpcConnector: "pass-connector",
-    vpcConnectorEgressSettings: "ALL_TRAFFIC",
-  })
-  // VPC 에 등록된 동일한 지역어야 한다.
-  .region("asia-northeast3")
-  .https.onRequest(async (request, response) => {
-    const auth = await NiceApi.requestCryptToken(Config.accessToken);
-    NiceApi.generateSymmetricKey(auth);
-    const encrypted = NiceApi.encryptData(auth);
-    const integrity_value = NiceApi.hmac256(encrypted, auth);
-    // const url = `https://nice.checkplus.co.kr/CheckPlusSafeModel/service.cb?m=service&token_version_id=${auth.crypto.dataBody.token_version_id}&enc_data=${encrypted}&integrity_value=${integrity_value}`;
-    // response.redirect(url);
+// VPC Connector 를 사용해서 고정 IP 를 사용한다.
+    .runWith({
+      vpcConnector: "pass-connector",
+      vpcConnectorEgressSettings: "ALL_TRAFFIC",
+    })
+// VPC 에 등록된 동일한 지역어야 한다.
+    .region("asia-northeast3")
+    .https.onRequest(async (request, response) => {
+    // 암호화  토큰 요청. (암호화 키 생성)
+    // 이 때, accessToken 은 미리 환경변수로 설정해둔다. (50년간 키 사용 가능.)
+      const auth = await NiceApi.requestCryptToken(Config.accessToken);
 
-    // 주의: redirect() 하면 동작하지 않는다 (redirect 는 30% 확률로 실패한다). 반드시 FORM submit() 으로 해야 한다.
+      // 암호화 토큰을 바탕으로 암호화 키 생성
+      NiceApi.generateSymmetricKey(auth);
+      // 생성한 암호화 키로, 데이터 암호화
+      // 요청 번호, sitecode, return url, 개발자가 직접 정의하는 상태 코드, authtype 등을 암호화.
+      const encrypted = NiceApi.encryptData(auth);
+      // Hmac 무결성 검증을 위한 코등 생성
+      const integrity_value = NiceApi.hmac256(encrypted, auth);
 
-    auth.encrypted = encrypted;
-    auth.integrity_value = integrity_value;
-    // token version id 를 키로 해서, auth 정보를 저장한다. 인증 후, 복호화 할 때 사용.
-    await Firebase.niceApiCol()
-      .doc(auth.crypto.dataBody.token_version_id)
-      .set(auth);
-    response.send(`
+      // 암호화 정보를 Firestore 에 보관
+      //
+      // 안전하게, Security rule 자체를 적용하지 않으면 된다. Cloud function 은 rule 없이 참조 가능.
+      // 문서를 token version id 를 키로 해서, 암호화(auth) 정보를 저장. 인증 후 -> 복호화 할 때 사용.
+      auth.encrypted = encrypted;
+      auth.integrity_value = integrity_value;
+      await Firebase.niceApiCol()
+          .doc(auth.crypto.dataBody.token_version_id)
+          .set(auth);
+
+      // 인증창을 띄우기 위해, FORM 을 만들고 전송한다. response.redirect() 는 안된다.
+      response.send(`
 <html>
 <body>
 <form name="form_chk" id="form_chk" method="get" action="https://nice.checkplus.co.kr/CheckPlusSafeModel/checkplus.cb">
@@ -53,56 +69,62 @@ export const niceAuth = functions
 </body>
 </html>
     `);
-  });
+    });
 
 /**
- * 인증 후, RETURN URL 에 의해 호출되는 콜백 함수
+ * 나이스 API 표준 인증창에서 성공적으로 인증 후, (RETURN URL 에 의해) 호출되는 함수
  *
- * 복호 후, integrity_value 문서를 삭제한다.
+ * 나이스 API 가 사용자 정보를 암호화해서 보내주는데, 이를 복호화한다.
+ *
+ * 참고로, 암호화 할 때 저장해 놓은 auth 문서 (문서 키: integrity_value) 를 복호화 후 삭제해도 된다.
+ * 하지만, 용량이 크지 않고 (사용자 인증 회 수가 많지 않고) Security rules 을 작성하지 않으면 안전하므로
+ * 굳이 삭제하지 않는다. (물론 원한다면 삭제해도 된다.)
+ *
+ * 반드시 고정 IP 를 써야 한다.
  */
 export const niceAuthCallback = functions
-  .runWith({
-    vpcConnector: "pass-connector",
-    vpcConnectorEgressSettings: "ALL_TRAFFIC",
-  })
-  .region("asia-northeast3")
-  .https.onRequest(async (request, response) => {
-    const token_version_id = request.query.token_version_id as string;
-    const doc = await Firebase.niceApiCol().doc(token_version_id).get();
-    const auth = doc.data() as AuthData;
-    // console.log("auth: ", auth);
-    const decrypted = NiceApi.decryptData(
+    .runWith({
+      vpcConnector: "pass-connector",
+      vpcConnectorEgressSettings: "ALL_TRAFFIC",
+    })
+    .region("asia-northeast3")
+    .https.onRequest(async (request, response) => {
+    // 암호화 정보를 저장한 문서를 읽는다.
+      const token_version_id = request.query.token_version_id as string;
+      const doc = await Firebase.niceApiCol().doc(token_version_id).get();
+      const auth = doc.data() as AuthData;
+
+      // 복호화
+      const decrypted = NiceApi.decryptData(
       request.query.enc_data as string,
       auth
-    );
-    const json = JSON.parse(decrypted);
-    Config.trace(
-      "index.ts::niceAuthCallback(); Decrypted data in json: ",
-      json
-    );
+      );
+      const json = JSON.parse(decrypted);
+      Config.trace("niceAuthCallback(); Decrypted data in json: ", json);
 
-    const data = {
-      display_name: "",
-      name: decodeURIComponent(json.utf8_name),
-      birthday: json.birthdate,
-      phone_number: json.mobileno ?? "",
-      gender: json.gender == "1" ? "M" : "F",
-    } as UserData;
+      // 사용자 정보
+      const data = {
+        display_name: "",
+        name: decodeURIComponent(json.utf8_name),
+        birthday: json.birthdate,
+        phone_number: json.mobileno ?? "",
+        gender: json.gender == "1" ? "M" : "F",
+      } as UserData;
 
-    Config.trace(
-      "index.ts::niceAuthCallback(); -> going to create or login with data;",
-      data
-    );
-    const user = await Firebase.createUser(data);
+      // 사용자 정보를 바탕으로 회원 가입 or 로그인
+      Config.trace("niceAuthCallback(); User create or login;", data);
+      const user = await Firebase.createUser(data);
 
-    const customToken = await admin.auth().createCustomToken(user.uid);
+      // Custom token 생성
+      const customToken = await admin.auth().createCustomToken(user.uid);
 
-    const link = Config.dynamicLink.replace("PatchToken", customToken);
+      // 앱/웹으로 Custom token 전달할 Dynamic Link
+      // Dynamic Link 를 쉽게 만드려면, Firebase 에서 생성할 때, 임시 경로로 생성해서, 복사하여 수정한다.
+      // Dynamic Link 에서 `PatchToken` 부분을 Custom token 으로 치환하므로, 링크에 적절이 포함되어 있어야 한다.
+      const link = Config.dynamicLink.replace("PatchToken", customToken);
 
-    Config.trace("Dynamic Link", link);
+      Config.trace("Dynamic Link", link);
 
-    // 인증 & 복호화 & 회원 가입 or 로그인 후, 앱/웹으로 이동 할 때, URL redirect 한다.
-    // 참고, 인증을 시작 할 때, 표준인증창 열 때는, redirect 를 하면 오류가 많이 난다.
-    // 주의, In-App Browser 로 실행을 하면 동작하지 않는다. 반드시 iOS 에서 external browser 로 실행을 해야 한다.
-    response.redirect(link);
-  });
+      // 인증 -> 복호화 -> 회원 가입 or 로그인 -> Custom token 생성 -> 앱/웹으로 Custom token 전달(URL redirect)
+      response.redirect(link);
+    });
