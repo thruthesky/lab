7e15dd404f1b3046004b2a626b0af23390c0f3c8
JaeHo Song <thruthesky@gmail.com>
2023-03-31 12:46:16 +0900

    test run

--- firebase/cloud-functions/ff-security-check/functions/src/index.ts
+++ firebase/cloud-functions/ff-security-check/functions/src/index.ts
@@ -1,6 +1,8 @@
 import * as functions from "firebase-functions";
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import axios, { AxiosResponse } from "axios";
+// import { auth } from "firebase-admin";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -24,7 +26,7 @@ export const test = functions.https.onRequest((request, response) => {
                 Enter your site url:
             </h1>
             <form action="https://us-central1-flutter-flow-korea.cloudfunctions.net/run" method="POST">
-                <input type="text" name="url" />
+                <input type="text" name="url" size="48" />
                 <input type="submit" value="Submit" />
             </form>
             </div>
@@ -35,22 +37,91 @@ export const test = functions.https.onRequest((request, response) => {
 
 export const run = functions.https.onRequest(async (request, response) => {
   // / get main.dart
+  const url = request.body.url;
+  let res: AxiosResponse;
+  try {
+    res = await axios.get(url);
+  } catch (e) {
+    console.log("error on getting the site: axios.get(" + url + ")");
+    console.log(e);
+    // response.send("error on axios.get(url)");
+    throw e;
+  }
+
+  const html = res.data;
+
+  const regex = /https:.*main\.dart\.js/gm;
+
+  const jsUrl: string = regex.exec(html) as any;
+  try {
+    res = await axios.get(jsUrl);
+  } catch (e) {
+    console.log("error on getting the main.dart.js: axios.get(" + jsUrl + ")");
+    console.log(e);
+    // return response.send("error on axios.get(url)");
+    throw e;
+  }
+
+  //   console.log(res.data);
+
+  const js = res.data;
+
+  const securyRegex = /\(".*\.appspot.com"\)\)/gm;
+  const securyInfo: string = securyRegex.exec(js) as any;
+  console.log(securyInfo);
+
+  const parts = securyInfo
+    .toString()
+    .replace(/\"/g, "")
+    .replace(/\(/g, "")
+    .replace(/\)/g, "")
+    .split(",");
+
+  const apiKey = parts[0];
+  const appId = parts[1];
+  const authDomain = parts[2];
+  //   const databaseURL = parts[3];
+  //   const messagingSenderId = parts[4];
+  const projectId = parts[6];
+  //   const storageBucket = parts[7];
+
+  const info = `
+    <h1>Firebase Security Test</h1>
+    <small>url</small>
+    <div>${request.body.url}</div>
+    <small>main.dart.js url</div>
+    <div>${jsUrl}</div>
+    <small>secury info parts</small>
+    <div>${securyInfo}</div>
+    <div>0 ${parts[0]}</div>
+    <div>1 ${parts[1]}</div>
+    <div>2 ${parts[2]}</div>
+    <div>3 ${parts[3]}</div>
+    <div>4 ${parts[4]}</div>
+    <div>5 ${parts[5]}</div>
+    <div>6 ${parts[6]}</div>
+    <div>7 ${parts[7]}</div>
+    
+    `;
+
+  console.log(info);
+
   // / parse access
   // Initialize another app
   const app = initializeApp({
-    apiKey: "AIzaSyBz2OFyac4LdNAA8jomtLlpSdBsO1BWyjY",
-    authDomain: "withcenter-test-2.firebaseapp.com",
+    apiKey: apiKey,
+    authDomain: authDomain,
     databaseURL: "https://withcenter-test-2-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "withcenter-test-2",
+    projectId: projectId,
     storageBucket: "withcenter-test-2.appspot.com",
     messagingSenderId: "817502397544",
-    appId: "1:817502397544:web:c3089782f014d4032487dd",
+    appId: appId,
   });
 
   const db = getFirestore(app);
   const citiesCol = collection(db, "users");
   const citySnapshot = await getDocs(citiesCol);
-  const cityList = citySnapshot.docs.map((doc) => {
+  const userList = citySnapshot.docs.map((doc) => {
     const data = doc.data();
 
     return {
@@ -61,6 +132,7 @@ export const run = functions.https.onRequest(async (request, response) => {
       photoUrl: data.photoUrl,
     };
   });
-  console.log(cityList);
-  response.send(cityList);
+
+  //   console.log(userList);
+  response.send(userList);
 });
