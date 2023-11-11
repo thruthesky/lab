1f02c4c1b160125e528dec4678a529a4b8e02a47
JaeHo Song <thruthesky@gmail.com>
2023-11-11 18:59:42 +0900

    update on firebase json download

--- firebase/firestore-json-download/index.ts
+++ firebase/firestore-json-download/index.ts
@@ -1,35 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { collection, getDocs, getFirestore, query } from "firebase/firestore";
+import * as admin from "firebase-admin";
 
-import * as fs from 'node:fs';
+const app = admin.initializeApp({
+    credential: admin.credential.cert(process.argv[2]),
+});
 
-
-
-// Initialize Firebase from the config file specified in the command line by reading the config file with fs.readFileSync and parsing the JSON string with JSON.parse
-const config = JSON.parse( fs.readFileSync( process.argv[2] ).toString() );
-
-
-
-
-// Initialize Firebase
-const app = initializeApp(
-    config
-);
-
-
-const db = getFirestore();
-const col = collection(db, "users");
-
+const db = admin.firestore();
+const col = db.collection("users");
 
 (async () => {
- const snapshot = await getDocs(query(col));
+ const snapshot = await col.get();
  if (snapshot.empty) {
    console.log("No matching documents.");
    return;
  }
-
-
+ 
  snapshot.forEach((doc) => {
    console.log(doc.id, "=>", doc.data());
  });
